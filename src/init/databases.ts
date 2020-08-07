00bdd2a8253636c6c591a0c6f4a776efab73a5cb
Carlos Macêdo <carlosmacedo025@gmail.com>
2020-08-07 19:24:41 -0300

    Database with mongoose

--- src/init/databases.ts
+++ src/init/databases.ts
@@ -1,3 +1,23 @@
+import mongoose from 'mongoose';
+import config from 'config';
+import Debug from 'debug';
+const dbDebugger = Debug('app:db');
+
 export function initDatabases(): void {
-  // TODO
+  let dbUrl: string = config.get('db.url');
+  const dbName: string = config.get('db.name');
+  const dbPassword: string = config.get('db.password');
+  dbUrl = dbUrl.replace('<dbname>', dbName);
+  dbUrl = dbUrl.replace('<password>', dbPassword);
+  console.log(dbUrl);
+  mongoose
+    .connect(dbUrl, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: true
+    })
+    .then((c) => {
+      dbDebugger(c.connections);
+      dbDebugger('DB connection successful!');
+    });
 }
